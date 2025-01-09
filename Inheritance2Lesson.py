5b7df05f46f70c842cb64a7102a57e6977e4c033
Nizaminem <nagarizaev1994@gmail.com>
2025-01-09 19:21:22 +0300

    Съедобное, несъедобное

---
+++ Inheritance2Lesson.py
@@ -0,0 +1,51 @@
+class Vehicle:
+
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+
+    def __init__(self, owner, __model, __color, __engine_power):
+        self.owner = owner
+        self.__model = __model
+        self.__engine_power = __engine_power
+        self.__color = __color
+
+    def get_model(self):
+        return f'Модель: {self.__model}'
+
+    def get_horsepower(self):
+        return f'Мощность двигателя: {self.__engine_power}'
+
+    def get_color(self):
+        return f'Цвет транспорта: {self.__color}'
+
+    def print_info(self):
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
+        print(f'Владелец: {self.owner}')
+
+    def set_color(self, new_color):
+        if new_color.lower() in map(str.lower, self.__COLOR_VARIANTS):
+            self.__color = new_color
+        else:
+            print(f'Нельзя сменить цвет на {new_color}')
+
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+    def __init__(self, owner, __model, __color, __engine_power):
+        super().__init__(owner, __model, __color, __engine_power)
+
+# Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+vehicle = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+
+# Изначальные свойства
+vehicle.print_info()
+
+# Меняем свойства (в т.ч. вызывая методы)
+vehicle.set_color('Pink')
+vehicle.set_color('BLACK')
+vehicle.owner = 'Vasyok'
+
+# Проверяем что поменялось
+vehicle.print_info()
